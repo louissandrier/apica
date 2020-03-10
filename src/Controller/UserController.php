9a93faec5d35ea6eb92a3ec026fd0827d697fe32
pierre <mouchelp72531@gmail.com>
2020-03-10 14:57:41 +0100

    add user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,22 +7,22 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
     /**
      *
-     * @Route("/user", name="create_user")
+     * @Route("/user", name="create_user", methods={"POST"})
      */
-    public function createUser(): Response
+    public function addUser(Request $request): Response
     {
-
         $entityManager = $this->getDoctrine()->getManager();
 
         $user = new User();
-        $user->setUsername('Adminn');
-        $user->setPassword('super-admin');
-        $user->setRole('1');
+        $user->setUsername($_POST['username']);
+        $user->setPassword($_POST['password']);
+        $user->setRole($_POST['role']);
 
         $entityManager->persist($user);
         $entityManager->flush();
--- src/Migrations/Version20200310131458.php
+++ src/Migrations/Version20200310131458.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200310102551 extends AbstractMigration
+final class Version20200310131458 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -25,10 +25,9 @@ final class Version20200310102551 extends AbstractMigration
         $this->addSql('CREATE TABLE citation (id INT AUTO_INCREMENT NOT NULL, film_id INT NOT NULL, personnage_id INT NOT NULL, citation VARCHAR(1000) NOT NULL, INDEX IDX_FABD9C7E567F5183 (film_id), INDEX IDX_FABD9C7E5E315342 (personnage_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE film (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE personnage (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role INT NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE citation ADD CONSTRAINT FK_FABD9C7E567F5183 FOREIGN KEY (film_id) REFERENCES film (id)');
         $this->addSql('ALTER TABLE citation ADD CONSTRAINT FK_FABD9C7E5E315342 FOREIGN KEY (personnage_id) REFERENCES personnage (id)');
-        $this->addSql('DROP TABLE movie');
     }
 
     public function down(Schema $schema) : void
@@ -38,7 +37,6 @@ final class Version20200310102551 extends AbstractMigration
 
         $this->addSql('ALTER TABLE citation DROP FOREIGN KEY FK_FABD9C7E567F5183');
         $this->addSql('ALTER TABLE citation DROP FOREIGN KEY FK_FABD9C7E5E315342');
-        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE citation');
         $this->addSql('DROP TABLE film');
         $this->addSql('DROP TABLE personnage');
