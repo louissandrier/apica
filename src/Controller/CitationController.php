31aeddb0c6e2fa0eaca6ba5546adb4b316aeac24
lsand <louis.sandrier@imie.fr>
2020-03-10 12:02:19 +0100

    feature controller add new user

---
+++ src/Controller/CitationController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lsand
+ * Date: 10/03/2020
+ * Time: 11:56
+ */
+
+namespace App\Controller;
+
+
+class CitationController
+{
+
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     *
+     * @Route("/user", name="create_user")
+     */
+    public function createUser(): Response
+    {
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setUsername('Admin');
+        $user->setPassword('super-admin');
+        $user->setRole('1');
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new Response('Saved new user with id '.$user->getId());
+    }
+
+}
