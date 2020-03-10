7ebd6221202b12416bf2977822d49d3b6461a399
pierre <mouchelp72531@gmail.com>
2020-03-10 15:55:58 +0100

    test

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,7 +7,6 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
@@ -15,7 +14,7 @@ class UserController extends AbstractController
      *
      * @Route("/user", name="create_user", methods={"POST"})
      */
-    public function addUser(Request $request): Response
+    public function addUser(): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
 
@@ -28,6 +27,23 @@ class UserController extends AbstractController
         $entityManager->flush();
 
         return new Response('Saved new user with id '.$user->getId());
+    }
+
+    /**
+     *
+     * @Route("/connexion", name="get_user_by_username", methods={"POST"})
+     */
+    public function authenticateUser(): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+
+        $user = $repository->findOneBy(['username' => $_POST['username']]);
+
+        if($user->getPassword() == $_POST['password']){
+          var_dump($user);
+          return new Response();
+        }
+
     }
 
 }
